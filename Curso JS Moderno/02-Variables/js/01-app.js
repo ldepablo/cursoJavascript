0a961a9400e2f8d2e6eb601d9c2385b9ceaadb4f
Luis de Pablo <luis.depablo@santander.co.uk>
2023-03-14 18:47:05 +0100

    13. Todo lo que debes saber de las variables con var

---
+++ Curso JS Moderno/02-Variables/js/01-app.js
@@ -0,0 +1,9 @@
+var producto = "Monitor de 24 pulgadas"
+console.log(producto)
+producto = 'Monitor de 19 pulgadas'
+console.log(producto)
+producto = 10
+console.log(producto)
+
+var disponible
+disponible = true
