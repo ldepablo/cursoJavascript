306f2418e01410b2efc0d48cb61ec6e9f1e0bc61
Luis de Pablo <luis.depablo@santander.co.uk>
2023-03-14 19:24:06 +0100

    21. String Methods - Repeat y Split

---
+++ Curso JS Moderno/03-Strings/js/06-app.js
@@ -0,0 +1,10 @@
+const producto = 'Monitor de 20 pulgadas'
+
+// .repeat
+
+const texto = 'En promocion'.repeat(3)
+console.log(texto)
+
+// .split
+const actividad = 'estoy aprendiendo javascript moderno'
+console.log(actividad.split(' '));
