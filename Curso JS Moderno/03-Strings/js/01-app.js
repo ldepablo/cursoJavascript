1d34fbaf77e72b693f391930e8746dde5228de5b
Luis de Pablo <luis.depablo@santander.co.uk>
2023-03-14 18:59:04 +0100

    16. Crear Strings en JavaScript

---
+++ Curso JS Moderno/03-Strings/js/01-app.js
@@ -0,0 +1,7 @@
+const producto = 'Monitor de 20"'
+const producto2 = String("Monitor de 20\"")
+const producto3 = new String('Monitor de 20 pulgadas')
+
+console.log(producto)
+console.log(producto2)
+console.log(producto3)
