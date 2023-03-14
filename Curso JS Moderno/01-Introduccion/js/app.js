1df6bfbad882bed744f26452ff98fd00f179f8c3
Luis de Pablo <luis.depablo@santander.co.uk>
2023-03-14 16:43:05 +0100

    9. Comentar el Código en JavaScript

--- Curso JS Moderno/01-Introduccion/js/app.js
+++ Curso JS Moderno/01-Introduccion/js/app.js
@@ -1,2 +1,9 @@
 const nombre = prompt("Tu nombre")
 document.querySelector('.contenido').innerHTML = `${nombre} está aprendiendo`
+
+// Comentario de una línea
+/*
+    Comentario
+    de varias
+    líneas
+*/
