86c6c04ffe492651b276a34457479223e61985b9
Luis de Pablo <luis.depablo@santander.co.uk>
2023-03-14 19:12:38 +0100

    19. Cortar Espacios en Blanco de un String

---
+++ Curso JS Moderno/03-Strings/js/04-app.js
@@ -0,0 +1,7 @@
+let producto = '     Monitor de 20       '
+
+console.log(producto.length)
+producto = producto.trimStart()
+console.log(producto.length)
+producto = producto.trimEnd()
+console.log(producto.length)
