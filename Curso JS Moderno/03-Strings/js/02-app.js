a0139227f7e677b981e81b9e6f7600d37a9d1941
Luis de Pablo <luis.depablo@santander.co.uk>
2023-03-14 19:03:13 +0100

    17. String Methods - includes y length

---
+++ Curso JS Moderno/03-Strings/js/02-app.js
@@ -0,0 +1,9 @@
+const producto = 'Monitor de 20'
+
+console.log(producto.length)
+
+console.log(producto.indexOf('tor'))
+
+console.log(producto.includes('Tablet'))
+console.log(producto.includes('monitor'))
+console.log(producto.includes('Monitor'))
