2cbd9c3818d290eb325eec202e207ff49304e1d0
Luis de Pablo <luis.depablo@santander.co.uk>
2023-03-18 13:11:12 +0100

    99. getElementById

---
+++ Curso JS Moderno/13-DOM/js/03-scripts.js
@@ -0,0 +1,7 @@
+// si hay varios, devuelve el primero que encuentre
+const formulario = document.getElementById('formulario')
+console.log(formulario);
+
+// Si no existe, devuelve false
+const noExiste = document.getElementById('noExiste')
+console.log(noExiste);
