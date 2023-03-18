668e191621741739fcad3ba6f520ddf2a078e900
Luis de Pablo <luis.depablo@santander.co.uk>
2023-03-18 13:03:38 +0100

    97. Intro a DOM

---
+++ Curso JS Moderno/13-DOM/js/01-scripts.js
@@ -0,0 +1,20 @@
+
+let elemento = window
+elemento = document
+elemento = document.head
+elemento = document.body
+elemento = document.forms
+elemento = document.forms[0]
+elemento = document.forms[0].id
+elemento = document.forms[0].method
+elemento = document.forms[0].classList
+elemento = document.forms[0].action
+
+elemento = document.links
+elemento = document.links[4]
+elemento = document.links[4].classList
+elemento = document.links[4].className
+
+elemento = document.images
+
+console.log(elemento);
