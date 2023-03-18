bcef6e35fe4f153155fd60798b666e8411b23ce8
Luis de Pablo <luis.depablo@santander.co.uk>
2023-03-18 13:21:59 +0100

    101. querySelectorAll

---
+++ Curso JS Moderno/13-DOM/js/05-scripts.js
@@ -0,0 +1,7 @@
+// Devuelve todos
+const cards = document.querySelectorAll('.card')
+console.log(cards);
+
+// Si no existe, devuelve array vacío
+const noExiste = document.querySelectorAll('.noExiste')
+console.log(noExiste);
